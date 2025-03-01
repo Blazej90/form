789d554462fabbd5cfe8a87697780e8b50ff631a
Błażej Bartoszewski <blazejbart@gmail.com>
2025-03-01 20:33:24 +0100

    chore(page): remove empty <div> from page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,5 @@ import React from "react";
 import { Layout } from "@/components/Layout";
 
 export default function Page() {
-  return (
-    <Layout>
-      <div></div>
-    </Layout>
-  );
+  return null;
 }
