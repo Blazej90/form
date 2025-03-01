2234728558cf3477c0348ea59430eb86302704cb
Błażej Bartoszewski <blazejbart@gmail.com>
2025-03-01 23:40:52 +0100

    refactor(layout): switch to server model and create layout-client.tsx component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,6 @@
-"use client";
-
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Geist, Geist_Mono } from "next/font/google";
-import { ThemeProvider } from "@/components/theme-provider";
-import Modal from "react-modal";
-import { LeftPanel } from "@/components/left-panel/left-panel";
-import { RightPanel } from "@/components/right-panel/right-panel";
-import { Field } from "@/types/types";
+import { LayoutClient } from "@/components/layout-client";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -22,47 +16,12 @@ const geistMono = Geist_Mono({
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
-  const [title, setTitle] = useState<string>("");
-  const [fields, setFields] = useState<Field[]>([]);
-  const [activeCard, setActiveCard] = useState<string | null>(null);
-
-  const resetForm = () => {
-    setFields([]);
-    setTitle("");
-    setActiveCard(null);
-  };
-
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      Modal.setAppElement("#__next");
-    }
-  }, []);
-
   return (
     <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <div id="__next">
-            <div className="flex h-screen">
-              <LeftPanel
-                title={title}
-                setTitle={setTitle}
-                fields={fields}
-                setFields={setFields}
-                activeCard={activeCard}
-                setActiveCard={setActiveCard}
-              />
-              <RightPanel title={title} fields={fields} resetForm={resetForm} />
-            </div>
-          </div>
-        </ThemeProvider>
+        <LayoutClient>{children}</LayoutClient>
       </body>
     </html>
   );
---
+++ src/components/layout-client.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import Modal from "react-modal";
+import { ThemeProvider } from "@/components/theme-provider";
+import { LeftPanel } from "@/components/left-panel/left-panel";
+import { RightPanel } from "@/components/right-panel/right-panel";
+import { Field } from "@/types/types";
+
+export const LayoutClient: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const [title, setTitle] = useState<string>("");
+  const [fields, setFields] = useState<Field[]>([]);
+  const [activeCard, setActiveCard] = useState<string | null>(null);
+
+  const resetForm = () => {
+    setFields([]);
+    setTitle("");
+    setActiveCard(null);
+  };
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      Modal.setAppElement("#__next");
+    }
+  }, []);
+
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <div id="__next">
+        <div className="flex h-screen">
+          <LeftPanel
+            title={title}
+            setTitle={setTitle}
+            fields={fields}
+            setFields={setFields}
+            activeCard={activeCard}
+            setActiveCard={setActiveCard}
+          />
+          <RightPanel title={title} fields={fields} resetForm={resetForm} />
+        </div>
+      </div>
+    </ThemeProvider>
+  );
+};
